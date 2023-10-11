f6e720bbe545e69ba3ce4904202a5d76657708e0
22521518 <22521518@gm.uit.edu.vn>
2023-10-11 09:37:09 +0700

    trash commit

--- GameXepGach/MainGame/BasicForm.cs
+++ GameXepGach/MainGame/BasicForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MainGame
 {
-    public partial class Form1 : Form
+    public partial class BasicForm : Form
     {
-        public Form1()
+        public BasicForm()
         {
             InitializeComponent();
         }
