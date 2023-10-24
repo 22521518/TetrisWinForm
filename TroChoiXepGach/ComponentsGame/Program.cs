ce59ba17d940a01b2ab25e28812ef6292f0cae9e
22521518 <22521518@gm.uit.edu.vn>
2023-10-24 17:38:14 +0700

    commit comment code for not error

--- TroChoiXepGach/ComponentsGame/Program.cs
+++ TroChoiXepGach/ComponentsGame/Program.cs
@@ -17,30 +17,31 @@ namespace ComponentsGame
         [STAThread]
         static void Main()
         {
-            string datapath = "Database.db";
-            if (!File.Exists(datapath))
-                SQLiteConnection.CreateFile(datapath);
-            using (SQLiteConnection con = new SQLiteConnection($"Data Source={datapath}; Version=3"))
-            {
-                con.Open();
-                using (SQLiteCommand cmd = new SQLiteCommand(con))
-                {
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS PLAYER (ID TINYINT PRIMARY KEY, NAME VARCHAR(16));";
-                    cmd.ExecuteNonQuery();
-                    cmd.CommandText = "INSERT INTO PLAYER (ID, NAME) VALUES (3, 'aloalo')";
-                    cmd.ExecuteNonQuery();
+            //BASIC CODE FOR DATABASE
+            //string datapath = "Database.db";
+            //if (!File.Exists(datapath))
+            //    SQLiteConnection.CreateFile(datapath);
+            //using (SQLiteConnection con = new SQLiteConnection($"Data Source={datapath}; Version=3"))
+            //{
+            //    con.Open();
+            //    using (SQLiteCommand cmd = new SQLiteCommand(con))
+            //    {
+            //        cmd.CommandText = "CREATE TABLE IF NOT EXISTS PLAYER (ID TINYINT PRIMARY KEY, NAME VARCHAR(16));";
+            //        cmd.ExecuteNonQuery();
+            //        cmd.CommandText = "INSERT INTO PLAYER (ID, NAME) VALUES (3, 'aloalo')";
+            //        cmd.ExecuteNonQuery();
 
-                    cmd.CommandText = "SELECT * FROM PLAYER";
-                    Console.WriteLine(cmd.CommandText);
-                    using (SQLiteDataReader reader = cmd.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Console.WriteLine($" {reader.GetInt32(0)}  {reader.GetString(1)}");
-                        }
-                    }
-                }
-            }
+            //        cmd.CommandText = "SELECT * FROM PLAYER";
+            //        Console.WriteLine(cmd.CommandText);
+            //        using (SQLiteDataReader reader = cmd.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                Console.WriteLine($" {reader.GetInt32(0)}  {reader.GetString(1)}");
+            //            }
+            //        }
+            //    }
+            //}
         }
     }
 }
